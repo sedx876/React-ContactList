ff41c6ecbab1c6cdde284b860bab1ef0d97ae3ad
sedx876 <swatkins876@gmail.com>
2020-10-02 09:18:45 -0400

    refactored app component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import React, { useState } from 'react';
 import './styles/App.css';
 import { Numbers } from './components/Numbers'
+import { Filter } from './components/Filter'
 
 
 export const App = () => {
 
+  const [word, setWord] = useState('')
   const [persons] = useState([
-    {name: "Sharon", number: "570-637-8765"},
-    {name: "Mike", number: "570-637-3043"},
-    {name: "Kendra", number: "570-637-2121"}
-  ])
+    {name: 'Sharon Watkins', number: '570-637-8765'},
+    {name: 'Mike Watkins', number: '570-637-3043'},
+    {name: 'Kendra Watkins', number: '570-637-2121'},
+    {name: 'Sam Watkins', number: '570-637-1212'},
+  ]);
+  const [filterDisplay, setFilterDisplay] = useState([])
 
-return (
-  <div>
-    <Numbers persons={persons} />
-  </div>
-)
+  const handleChange = e => {
+      setWord(e)
+      let oldList = persons.map(person => {
+        return { name: person.name.toLowerCase(), number: person.number}
+      })
+  
+      if (word !== '') {
+        let newList = []
+        
+        newList = oldList.filter(person => 
+          person.name.includes(word.toLowerCase())
+          )
+          setFilterDisplay(newList)
+      } else {
+        setFilterDisplay(persons)
+      }
+    }
+
+    return (
+      <div>
+        <Filter value={word} handleChange={e => handleChange(e.target.value)}/>
+        <Numbers persons={word.length < 1 ? persons : filterDisplay}/>
+      </div>
+    )
 
 }
 
